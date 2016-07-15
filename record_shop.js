117c25901b1c10f950e2ca17f7da1a23c40c7039
Russell Williams <russellwilliamscc@gmail.com>
2016-07-15 12:18:22 +0100

    adds record in to stock

--- record_shop.js
+++ record_shop.js
@@ -1,7 +1,14 @@
-var RecordShop = function(name, location) {
+var RecordShop = function(name, location, till) {
   this.name = name;
   this.location = location;
   this.stock = [];
+  this.till = till;
+}
+
+RecordShop.prototype = {
+  addRecord: function(record){
+    this.stock.push(record);
+  }
 }
 
 module.exports = RecordShop;
--- specs/record_shop_spec.js
+++ specs/record_shop_spec.js
@@ -10,6 +10,12 @@ describe( "RecordShop", function() {
 
     surfaceNoise = new RecordShop("Surface Noise", "Newcastle");
 
+    loveless = new Record("Loveless", "My Bloody Valentine", 9.99);
+    crooked = new Record("Crooked Rain, Crooked Rain", "Pavement", 7.99);
+    theWayItIs = new Record("The Way It Is", "Bruce Hornsby and the Range", 4.99);
+    vivaHate = new Record("Viva Hate", "Morrissey", 5.99);
+    tanglewoodNumbers = new Record("Tanglewood Numbers", "Silver Jews", 11.99);
+
   })
 
   it ("record shop has a title", function(){
@@ -23,5 +29,11 @@ describe( "RecordShop", function() {
   it ("record shop has NO stock", function(){
     assert.equal(0, surfaceNoise.stock.length)
   })
+
+  it ("book record in to stock", function() {
+    surfaceNoise.addRecord(loveless);
+    assert.equal(1, surfaceNoise.stock.length)
+  })
+
 
 })
