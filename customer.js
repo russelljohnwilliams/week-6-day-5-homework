0cbcbbf075ae6fe56467d5055ef2c158cfd9ce04
Russell Williams <russellwilliamscc@gmail.com>
2016-07-16 16:47:08 +0100

    adds customer buying

--- customer.js
+++ customer.js
@@ -13,12 +13,28 @@ Customer.prototype = {
     this.shoppingBag.push(record);
   },
 
-  buyRecord: function(record, callback){
-    callback(record);
-    this.wallet -= (_.toNumber(record.price));
+  buyRecord: function(record, shop){
     this.shoppingBag.push(record);
-  }//.bind( this )
+    this.wallet -= (_.toNumber(record.price));
+    shop.till += (_.toNumber(record.price));
+    var soldItem = _.findIndex(shop.stock, record);
+    _.pullAt(shop.stock, [soldItem]);
+  },
+
+  sellRecord: function(record, shop){
+    var soldItem = _.findIndex(this.shoppingBag, record);
+    var pulled = _.pullAt(this.shoppingBag, [soldItem]);
+    var price = _.map(pulled, 'price');
+    this.wallet += (_.toNumber(price));
+    console.log(shop.till)
+    shop.till -= (_.toNumber(price));
+    
+  }
+
+
+
 }
+
 
 module.exports = Customer;
 
--- specs/customer.spec.js
+++ specs/customer.spec.js
@@ -38,18 +38,42 @@ describe( "Customer", function(){
   it ("customer puts a record in shopping bag", function(){
     surfaceNoise.addRecord(loveless);
     surfaceNoise.addRecord(vivaHate);
-    console.log(surfaceNoise.stock);
-    bob.buyRecord(loveless, surfaceNoise.soldRecord);
-    console.log(surfaceNoise.stock);
+    bob.buyRecord(loveless, surfaceNoise);
     assert.equal(1, bob.shoppingBag.length)
   })
 
-  // it ("customer removes money from wallet", function(){
-  //   surfaceNoise.addRecord(loveless);
-  //   surfaceNoise.addRecord(vivaHate);
-  //   bob.buyRecord(loveless, surfaceNoise.soldRecord);
-  //   assert.equal(140.01, bob.wallet)
-  // })
+  it ("customer removes money from wallet", function(){
+    surfaceNoise.addRecord(loveless);
+    surfaceNoise.addRecord(vivaHate);
+    bob.buyRecord(loveless, surfaceNoise);
+    assert.equal(140.01, bob.wallet)
+  })
+
+  it ("record shop sells a record", function(){
+    surfaceNoise.addRecord(loveless);
+    surfaceNoise.addRecord(vivaHate);
+    // console.log(surfaceNoise.stock);
+    bob.buyRecord(loveless, surfaceNoise);
+    // console.log(surfaceNoise.stock);
+    assert.equal(1, surfaceNoise.stock.length)
+  })
+
+  it("record shop till gets some money", function(){
+    surfaceNoise.addRecord(loveless);
+    surfaceNoise.addRecord(vivaHate);
+    bob.buyRecord(loveless, surfaceNoise);
+    // console.log(surfaceNoise.till);
+    assert.equal(9.99, surfaceNoise.till)
+  })
+
+  it ("customer sells a record", function(){
+      surfaceNoise.addRecord(loveless);
+      bob.addRecord(loveless);
+      bob.addRecord(vivaHate)
+      bob.sellRecord(loveless, surfaceNoise);
+      assert.equal(1, bob.shoppingBag.length)
+  })
+
 
  })
 
